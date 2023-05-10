b2d04ebcd04373970329206dddf4b34f2c205ab8
shafik720 <shafikrasel5@gmail.com>
2023-05-11 00:34:20 +0600

    changing style

--- Module - 66/src/pages/AddProduct/AddProduct.js
+++ Module - 66/src/pages/AddProduct/AddProduct.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import spiderman from '../../assets/img/spiderman.jpg';
 
 const AddProduct = () => {
     return (
         <div className='w-2/3 mx-auto'>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="hero-content flex-col lg:flex-row gap-14">
                     <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <img src={spiderman} className="max-w-sm rounded-lg shadow-2xl" />
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
