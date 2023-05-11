b8836b04cd723fbabbe7af2d03cd68e6320aa52d
shafik720 <shafikrasel5@gmail.com>
2023-05-11 13:06:45 +0600

    working on homepage

--- Module - 66/src/pages/AddProduct/AddProduct.js
+++ Module - 66/src/pages/AddProduct/AddProduct.js
@@ -6,29 +6,29 @@ const AddProduct = () => {
         <div className='lg:w-2/3 mx-auto'>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col-reverse md:flex-row lg:flex-row gap-14 justify-between">
-                    <div className="text-center lg:text-left">
-                        <img src={spiderman} className="max-w-sm rounded-lg shadow-2xl" />
+                    <div className="text-center lg:text-left">  
+                        <img alt='' src={spiderman} className="max-w-sm rounded-lg shadow-2xl" />
                     </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-slate-500 shadow-black bg-base-100 ">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-slate-500  bg-base-100 ">
                         <div className="card-body ">
                             {/* <h2 className='font-bold text-2xl text-center text-slate-700 '>Add a New Product</h2> */}
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text font-semibold">Product Name</span>
                                 </label>
-                                <input type="text" placeholder="Product name" className="input border-2 rounded outline-none border-zinc-300" />
+                                <input type="text" placeholder="Product name" className="input border-2 rounded outline-none border-zinc-300 focus:border-blue-500  focus:outline-none " />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text font-semibold">Product Image Link</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input border-2 rounded outline-none border-zinc-300" />
+                                <input type="text" placeholder="password" className="input border-2 rounded outline-none border-zinc-300 focus:border-blue-500  focus:outline-none " />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text font-semibold">Price</span>
                                 </label>
-                                <input type="number" placeholder="Price" className="input border-2 rounded outline-none border-zinc-300" />
+                                <input type="number" placeholder="Price" className="input border-2 rounded outline-none border-zinc-300 focus:border-blue-500  focus:outline-none " />
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Add Product</button>
