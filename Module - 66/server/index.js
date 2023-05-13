ba920712d9aaf519718b8ceb4f307cc3163238db
shafik720 <shafikrasel5@gmail.com>
2023-05-13 10:56:12 +0600

    setup node server

---
+++ Module - 66/server/index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const app = express() ; 
+const port = process.env.PORT || 5000 ; 
+
+const cors = require('cors');
+
+app.use(express.json()); 
+app.use(cors());
+
+
+app.get('/', (req, res)=>{
+    res.send('Ok got it') ; 
+});
+
+
+app.listen(port, ()=>{
+    console.log('Listening to port 5000') ;
+})
