d8d80fc404cb526cab6c0b3859c8f51f42d33d0d
shafik720 <shafikrasel5@gmail.com>
2023-05-13 12:07:36 +0600

    added product with mongodb

--- Module - 66/server/index.js
+++ Module - 66/server/index.js
@@ -2,11 +2,46 @@
 const express = require('express');
 const app = express() ; 
 const port = process.env.PORT || 5000 ; 
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 
 app.use(express.json()); 
 app.use(cors());
+
+
+const uri = "mongodb+srv://shafikrasel5:sDHMGPmoZzUsbaVM@cluster0.hjqwlrm.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    try {
+      const database = client.db("PracticeDB");
+      const folder = database.collection("PracticeProduct");
+
+      app.post('/products/add', async(req, res)=>{
+        const body = req.body;
+        // console.log(body);
+        const result = await folder.insertOne(body);
+        res.send(result) ; 
+      })
+      // create a document to insert
+    //   const doc = {
+    //     title: "Record of a Shriveled Datum",
+    //     content: "No bytes, no problem. Just insert a document, in MongoDB",
+    //   }
+    //   const result = await folder.insertOne(doc);
+      
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 
 app.get('/', (req, res)=>{
--- Module - 66/src/pages/AddProduct/AddProduct.js
+++ Module - 66/src/pages/AddProduct/AddProduct.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import spiderman from '../../assets/img/spiderman.jpg';
 
 const AddProduct = () => {
+    const [user, setUser] = useState([]);
+
+    // --- adding product to mongodb
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let productName = e.target.productName.value;
+        let productImg = e.target.productImg.value;
+        let productPrice = e.target.productPrice.value;
+        setUser({ productName, productImg, productPrice });
+
+        fetch('http://localhost:5000/products/add', {
+            method : 'POST',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify({ productName, productImg, productPrice })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.insertedId){
+                window.alert('Product added Successfully ');
+                e.target.reset();
+            }else{
+                window.alert('There was an error adding the product');
+            }
+        })
+    }
     return (
         <div className='lg:w-2/3 mx-auto'>
             <div className="hero ">
@@ -12,27 +39,29 @@ const AddProduct = () => {
                     <div className="card flex-shrink-0 w-full max-w-sm  ">
                         <div className="card-body ">
                             {/* <h2 className='font-bold text-2xl text-center text-slate-700 '>Add a New Product</h2> */}
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text font-semibold">Product Name</span>
-                                </label>
-                                <input type="text" placeholder="Product name" className="input border-2 rounded outline-none border-zinc-300 focus:border-blue-500  focus:outline-none " />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text font-semibold">Product Image Link</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input border-2 rounded outline-none border-zinc-300 focus:border-blue-500  focus:outline-none " />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text font-semibold">Price</span>
-                                </label>
-                                <input type="number" placeholder="Price" className="input border-2 rounded outline-none border-zinc-300 focus:border-blue-500  focus:outline-none " />
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Add Product</button>
-                            </div>
+                            <form action="" onSubmit={handleSubmit}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-semibold">Product Name</span>
+                                    </label>
+                                    <input type="text" placeholder="Product name" className="input border-2 rounded outline-none border-zinc-300 focus:border-blue-500  focus:outline-none " name='productName' required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-semibold">Product Image Link</span>
+                                    </label>
+                                    <input type="text" placeholder="Link for the image" className="input border-2 rounded outline-none border-zinc-300 focus:border-blue-500  focus:outline-none " name='productImg' required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-semibold">Price</span>
+                                    </label>
+                                    <input type="number" placeholder="Price" className="input border-2 rounded outline-none border-zinc-300 focus:border-blue-500  focus:outline-none " name='productPrice' required />
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Add Product</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
