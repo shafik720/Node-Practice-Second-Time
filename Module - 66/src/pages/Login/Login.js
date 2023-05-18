ef90cef60587ba83cd96f3d30f3810801dbe2bf8
shafik720 <shafikrasel5@gmail.com>
2023-05-18 11:18:29 +0600

    Module 67 : working on style on login page

--- Module - 66/src/pages/Login/Login.js
+++ Module - 66/src/pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
@@ -7,11 +8,12 @@ const Login = () => {
         e.preventDefault();
     }
     return (
-        <div className="card  w-full ">
+        <div className="card  w-full mt-10">
             <div className="card-body w-full sm:w-full md:w-4/6 mx-auto lg:w-2/6">
-                {/* <h2 className='font-bold text-2xl text-center text-slate-700 '>Add a New Product</h2> */}
+                <h2 className='font-bold text-2xl text-center text-slate-700'>Login</h2>
+                <hr />
                 <form action="" onSubmit={handleSubmit}>
-                    <div className="form-control">
+                    <div className="form-control mt-5">
                         <label className="label">
                             <span className="label-text font-semibold">Your Email</span>
                         </label>
@@ -27,6 +29,7 @@ const Login = () => {
                         <button className="btn btn-primary">Login</button>
                     </div>
                 </form>
+                <h2 className=" mt-2">Don't have an account ? <span className='font-semibold text-cyan-500'><Link to={'/register'}>Register Here</Link></span> </h2>
             </div>
         </div>
     );
