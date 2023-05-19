2c8518b38a9e214ff438d3e298da30129d2e9c4b
shafik720 <shafikrasel5@gmail.com>
2023-05-19 12:18:59 +0600

    Module 67 : styling service section

--- Module - 66/src/pages/PrivateRoute/PrivateRoute.js
+++ Module - 66/src/pages/PrivateRoute/PrivateRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const PrivateRoute = ({children}) => {
     const[user, loading, error] = useAuthState(auth);
@@ -10,6 +11,16 @@ const PrivateRoute = ({children}) => {
         return <div>Checking Authentication</div>
     }
     if(!user){
+        toast.success('Login first to see this page', {
+            position: "bottom-center",
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
         return <Navigate to={'/login'} state={{from : location}}></Navigate>
     }
     return children ; 
--- Module - 66/src/pages/Services/Services.js
+++ Module - 66/src/pages/Services/Services.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Services = () => {
     return (
-        <div>
-            <h2>Service Page here</h2>
+        <div className='my-16 mx-auto w-5/6 py-5 grid lg:grid-cols-3 md:grid-cols-2 gap-y-5 '>
+            <div className="border-4 hover:shadow-lg">
+                <img src="https://i.ibb.co/T2cpBd5/888.jpg" className='w-full' alt="" />
+                <h2 className='font-bold text-xl my-4 px-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. </h2>
+                
+                <div className=" mt-5 flex justify-between items-center  rounded-md py-3 mx-3">
+                    <h2 className='m-0 font-bold text-2xl'>$45</h2>
+                    <button className='btn '>Book Service</button>
+                </div>
+            </div>
         </div>
     );
 };
